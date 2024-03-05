ffb12c0f220957f29dc7736120c91c2b3e7aa1a6
RodaynaElkhouly <126457443+RodaynaElkhouly@users.noreply.github.com>
2024-03-05 18:50:00 +0200

    Add files via upload

--- send/registerwindow.cpp
+++ send/registerwindow.cpp
@@ -44,6 +44,7 @@ void RegisterWindow::on_pushButton_Register2_clicked()
     QString retypedpassword = ui -> lineEditpasswordreg ->text();
     QString yearinput = ui -> lineEdityearreg ->text();
 
+    int x=yearinput.toInt();
     bool checkboxaction = ui -> checkBox ->isChecked();
     bool checkboxcomedy = ui -> checkBox_2 ->isChecked();
     bool checkboxromance = ui -> checkBox_3 ->isChecked();
@@ -55,57 +56,56 @@ void RegisterWindow::on_pushButton_Register2_clicked()
     bool radiobuttonadmin= ui->radioButton_3->isChecked();
     bool radiobuttonuser= ui->radioButton_4->isChecked();
     QString monthbutton=ui->comboBox->currentText();
-    QString yearbutton=ui->lineEdityearreg->text();
+    //QString yearbutton=ui->lineEdityearreg->text();
     QString daybutton=ui->lineEditdayreg->text();
-    QString  passlabel=ui->lineEditpasswordreg->text();
-    QString retypepass=ui->lineEditretyppasswordreg->text();
-    QString userbutton=ui->lineEditusernamereg->text();
+    //QString  passlabel=ui->lineEditpasswordreg->text();
+    //QString retypepass=ui->lineEditretyppasswordreg->text();
+    //QString userbutton=ui->lineEditusernamereg->text();
 
-    bool hasError =false;
+
+
+    bool check1=true;
+    bool check2=true;
+    bool check3=true;
+    bool check4=true;
 
     // Check if any of the conditions for registration is not met
 
+    for (int i = 0; i < 5; i++) {
+        if (usernameinput == usernames[i]) {
+            ui->username_error->setVisible(true);
 
+            check1=false;
+        }
+    }
     // Check if password matches the retyped password
     if (passwordinput != retypedpassword) {
         ui->password_error->setVisible(true);
-
+        check2=false;
     }
 
 
-
+    int userage = 2024 - x;
     // Check user age
-   int currentYear = QDate::currentDate().year();
-    int enteredYear = yearinput.toInt();
-    int userage= currentYear - enteredYear;
-    if (userage < 12) {
+    if((userage)<12)
+    {
         ui->age_error->setVisible(true);
-
-    }else
-         ui->age_error->setVisible(false);
+        check3=false;
+    }
+         //ui->age_error->setVisible(false);
 
     // Check if username already exists
-    for (int i = 0; i < 100; i++) {
-        if (usernameinput == usernames[i]) {
-            ui->username_error->setVisible(true);
-            hasError = true;
-            break;
-        }else
-             ui->username_error->setVisible(false);
-    }
-    if (((checkboxaction==false && checkboxcomedy==false && checkboxdrama==false &&
-          checkboxhorror==false && checkboxromance==false && checkboxother==false) ||
-         (passlabel=="") || (retypepass=="") || (userbutton=="") ||
-         (radiobuttonadmin==false && radiobuttonuser==false) ||
-         (radiobuttonmale==false && radiobuttonfemale==false) ||
-         (daybutton == "" || yearbutton==""))) {
-        ui->register_error->setVisible(true);
 
-    }
+         if((usernameinput=="")or(passwordinput=="")or(retypedpassword=="")or(daybutton=="")or(yearinput=="")or((radiobuttonmale==false)&&(radiobuttonfemale==false))or((radiobuttonuser==false)&&(radiobuttonadmin==false))or
+             ((checkboxaction==false)&&(checkboxcomedy==false)&&(checkboxromance==false)&&(checkboxdrama==false)&&(checkboxhorror==false)&&(checkboxother==false)))
+         {
+             ui->register_error->setVisible(true);
+             check4=false;
+         }
     else
         ui->register_error->setVisible(false);
     // Proceed with registration if no errors were found
-    if (!hasError) {
+    if(((check1)&&(check2)&&(check3)&&(check4))==true){
         // Perform registration
         hide();
         Welcomewindow* welcomewin = new Welcomewindow(this, usernameinput, userage);
